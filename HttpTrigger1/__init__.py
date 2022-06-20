2c9c35b75306743af561c4d757d0e4ca9f0e1a9b
Martin Noah <mnoah66@gmail.com>
2022-06-20 10:14:37 -0400

    first commit

---
+++ HttpTrigger1/__init__.py
@@ -0,0 +1,104 @@
+import logging
+
+import azure.functions as func
+import json
+import os
+
+import requests
+
+from helper import column_names
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    # Create CSV from combining dictionaries.
+    # data = [
+    #   header,row,with,keys,from,the,dictionaries,
+    #   all,the,values,from,the,dictionaries
+    # ]
+
+
+    req_body = req.get_json()
+
+    intro = req_body['intro']
+    
+    consumer = req_body['consumer']
+    eea = req_body['eea']    
+    facility = req_body['facility']
+    safety = req_body['safety']
+    logging.info(safety)
+    fleet = req_body['fleet']
+    support = req_body['support']
+    documentation = req_body['documentation']
+    medical = req_body['medical']
+    
+    cleaned_cols = column_names.cleaned_consumer_columns()
+   
+    data = {}
+
+    for key, value in intro.items():
+        data[key] = value
+    
+    if consumer:
+        for key, value in consumer.items():
+            try:
+                data[cleaned_cols['consumer_dict'][key]] = value
+            except:
+                data[key] = value
+    if eea:
+        for key, value in eea.items():
+            try:
+                data[cleaned_cols['eea_dict'][key]] = value
+            except:
+                data[key] = value
+    if facility:
+        for key, value in facility.items():
+            try:
+                data[cleaned_cols['facility_dict'][key]] = value
+            except:
+                data[key] = value
+    if safety:
+        for key, value in safety.items():
+            try:
+                data[cleaned_cols['safety_dict'][key]] = value
+            except:
+                data[key] = value
+    if fleet:
+        for key, value in fleet.items():
+            try:
+                data[cleaned_cols['fleet_dict'][key]] = value
+            except:
+                data[key] = value
+    if support:
+        for key, value in support.items():
+            try:
+                data[cleaned_cols['support_dict'][key]] = value
+            except:
+                data[key] = value
+    if documentation:
+        for key, value in documentation.items():
+            try:
+                data[cleaned_cols['docu_dict'][key]] = value
+            except:
+                data[key] = value
+    if medical:
+        for key, value in medical.items():
+            try:
+                data[cleaned_cols['medical_dict'][key]] = value
+            except:
+                data[key] = value
+
+    for key in list(data.keys()):
+        if "x0020" in key or "x003a" in key or "#Id" in key:
+            del data[key]
+            continue 
+        try:
+            data[key] = data[key]['Value']
+        except:
+            pass       
+
+    data = [data]
+    return func.HttpResponse(
+             json.dumps(data),
+             status_code=200
+        )
---
+++ helper/column_names.py
@@ -0,0 +1,98 @@
+import os
+import json
+import requests
+import logging
+
+
+def cleaned_consumer_columns():
+
+    client_id = os.getenv('client_id')
+    client_secret = os.getenv('client_secret')
+    tenant = os.getenv('tenant')
+    tenant_id = os.getenv('tenant_id')
+    client_id = client_id + '@' + tenant_id
+
+
+    data = {
+        'grant_type':'client_credentials',
+        'resource': "00000003-0000-0ff1-ce00-000000000000/" + tenant + ".sharepoint.com@" + tenant_id, 
+        'client_id': client_id,
+        'client_secret': client_secret,
+    }
+
+    headers = {
+        'Content-Type':'application/x-www-form-urlencoded'
+    }
+    # Make a POST request to LOGIN and get the token
+    r = requests.post("https://accounts.accesscontrol.windows.net/0e461760-99dd-4c07-8103-77f2c7465ea4/tokens/OAuth/2", data=data, headers=headers)
+    
+    json_data = json.loads(r.text)
+    # Add the bearer token to the header
+    headers = {
+        'Authorization': "Bearer " + str(json_data['access_token']),
+        'Accept':'application/json;odata=verbose',
+        'Content-Type': 'application/json;odata=verbose'
+    }
+
+    consumer = requests.get("https://arcessex.sharepoint.com/sites/QualityImprovement/_api/web/lists/getbytitle('ResQuarterlyConsumer')/Fields", headers=headers).json()
+    eea = requests.get("https://arcessex.sharepoint.com/sites/QualityImprovement/_api/web/lists/getbytitle('ResQuarterlyEEA')/Fields", headers=headers).json()
+    facility = requests.get("https://arcessex.sharepoint.com/sites/QualityImprovement/_api/web/lists/getbytitle('ResQuarterlyFacility')/Fields", headers=headers).json()
+    safety = requests.get("https://arcessex.sharepoint.com/sites/QualityImprovement/_api/web/lists/getbytitle('ResQuarterlySafety')/Fields", headers=headers).json()
+    fleet = requests.get("https://arcessex.sharepoint.com/sites/QualityImprovement/_api/web/lists/getbytitle('ResQuarterlyVehicleFleet')/Fields", headers=headers).json()
+    support = requests.get("https://arcessex.sharepoint.com/sites/QualityImprovement/_api/web/lists/getbytitle('ResQuarterlySupportDepartment')/Fields", headers=headers).json()
+    documentation = requests.get("https://arcessex.sharepoint.com/sites/QualityImprovement/_api/web/lists/getbytitle('ResQuarterlyDocumentation')/Fields", headers=headers).json()
+    medical = requests.get("https://arcessex.sharepoint.com/sites/QualityImprovement/_api/web/lists/getbytitle('ResQuarterlyMedDocStorage')/Fields", headers=headers).json()
+
+    lookup = {}
+
+    consumer_dict = {}
+    for item in consumer['d']['results']:
+        if not item['FromBaseType']: # Ignore the auto generated columns
+            consumer_dict[item['InternalName']] = item['Title']
+    lookup['consumer_dict'] = consumer_dict
+    
+    eea_dict={}
+    for item in eea['d']['results']:
+        if not item['FromBaseType']: # Ignore the auto generated columns
+            eea_dict[item['InternalName']] = item['Title']
+    lookup['eea_dict'] = eea_dict
+    
+    facility_dict={}
+    for item in facility['d']['results']:
+        if not item['FromBaseType']: # Ignore the auto generated columns
+            facility_dict[item['InternalName']] = item['Title']
+    lookup['facility_dict'] = facility_dict
+
+
+    safety_dict={}
+    for item in safety['d']['results']:
+        if not item['FromBaseType']: # Ignore the auto generated columns
+            safety_dict[item['InternalName']] = item['Title']
+    lookup['safety_dict'] = safety_dict
+    
+    fleet_dict={}
+    for item in fleet['d']['results']:
+        if not item['FromBaseType']: # Ignore the auto generated columns
+            fleet_dict[item['InternalName']] = item['Title'] 
+    lookup['fleet_dict'] = fleet_dict
+    
+    support_dict = {}
+    for item in support['d']['results']:
+        if not item['FromBaseType']: # Ignore the auto generated columns
+            support_dict[item['InternalName']] = item['Title']  
+    lookup['support_dict'] = support_dict
+
+    docu_dict = {}
+    for item in documentation['d']['results']:
+        if not item['FromBaseType']: # Ignore the auto generated columns
+            docu_dict[item['InternalName']] = item['Title']   
+    lookup['docu_dict'] = docu_dict
+
+    medical_dict = {}
+    for item in medical['d']['results']:
+        if not item['FromBaseType']: # Ignore the auto generated columns
+            medical_dict[item['InternalName']] = item['Title']
+    lookup['medical_dict'] = medical_dict
+    
+    
+    return lookup
